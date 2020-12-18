030d4b844d67bb5af6f74bf85a93665c27002438
Spandana932 <76148928+Spandana932@users.noreply.github.com>
2020-12-18 13:06:06 +0530

    Update Calci.java
    added some changes

--- fisrtProject/src/com/lti/gitdemo/Calci.java
+++ fisrtProject/src/com/lti/gitdemo/Calci.java
@@ -3,10 +3,10 @@ package com.lti.gitdemo;
 public class Calci {
 	
 public void add(int num1 , int num2) {
-	System.out.println(num1 + num2);
+	System.out.println("sum = " + str(num1 + num2));
 }
 
 public void diff(int num1 , int num2) {
-	System.out.println(num1 - num2);
+	System.out.println("difference = " + str(num1 - num2));
 }
 }
